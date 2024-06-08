20cb26351948831367e33e1a693a747f6d6e441a
D3k1 <dofodofonson@gmail.com>
2024-06-08 22:42:07 +0300

    paginations

--- BJGeDrenouille/goods/urls.py
+++ BJGeDrenouille/goods/urls.py
@@ -4,5 +4,6 @@ from goods import views
 app_name = 'catalog'
 urlpatterns = [
     path('<slug:company_slug>/', views.catalog, name='index'),
+    path('<slug:company_slug>/<int:page>/', views.catalog, name='index'),
     path('product/<slug:product_slug>/', views.product, name='product')
 ]
--- BJGeDrenouille/goods/views.py
+++ BJGeDrenouille/goods/views.py
@@ -1,16 +1,19 @@
 from django.shortcuts import get_list_or_404, render
-
+from django.core.paginator import Paginator
 from goods.models import Products
 
-def catalog(request, company_slug):
+def catalog(request, company_slug, page = 1):
     if company_slug == 'all':
         goods = Products.objects.all()
     else:
         goods = get_list_or_404(Products.objects.filter(company__slug = company_slug))
             
+    paginator = Paginator(goods, 6)
+    current_page = paginator.page(page)
     context = {
         'title': 'BJGeDrenouille - Каталог',
-        'goods': goods
+        'goods': current_page,
+        "slug_url": company_slug
     }
     return render(request, 'goods/catalog.html', context)
 
