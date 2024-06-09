57a305c6dbf606a4b534073f4c4f1eefb6f7558e
D3k1 <dofodofonson@gmail.com>
2024-06-09 03:09:08 +0300

    search

--- BJGeDrenouille/goods/urls.py
+++ BJGeDrenouille/goods/urls.py
@@ -3,6 +3,7 @@ from django.urls import path
 from goods import views
 app_name = 'catalog'
 urlpatterns = [
+    path('search/', views.catalog, name='search'),
     path('<slug:company_slug>/', views.catalog, name='index'),
     path('product/<slug:product_slug>/', views.product, name='product')
 ]
