0373cfbc7948c55f34c04f1faccd928d0ef7665f
D3k1 <dofodofonson@gmail.com>
2024-06-09 23:04:39 +0300

    admin panel edited

--- BJGeDrenouille/goods/admin.py
+++ BJGeDrenouille/goods/admin.py
@@ -5,7 +5,12 @@ from goods.models import Companies, Products
 @admin.register(Companies)
 class CompaniesAdmin(admin.ModelAdmin):
     prepopulated_fields = {'slug': ('name',)}
+    list_display = ['name']
 
 @admin.register(Products)
 class ProductsAdmin(admin.ModelAdmin):
     prepopulated_fields = {'slug': ('name',)}
+    list_display = ['name', 'quantity', 'price', 'discount']
+    list_editable = ['discount']
+    search_fields = ['name', 'description']
+    list_filter = ['discount', 'quantity', 'company']
