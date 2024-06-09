8a4bbcc2c5a0c68b5625cc2a9f5e80c90e59b8a9
D3k1 <dofodofonson@gmail.com>
2024-06-09 20:22:11 +0300

    cart done

--- BJGeDrenouille/carts/urls.py
+++ BJGeDrenouille/carts/urls.py
@@ -1,11 +1,11 @@
 from django.contrib import admin
 from django.urls import path
-from goods import views
+from carts import views
 
 app_name = 'carts'
 
 urlpatterns = [
-    path('cart_add/<int:product_id>', views.catalog, name='cart_add'),
-    path('cart_change/<int:product_id>', views.catalog, name='cart_change'),
-    path('cart_remove/<int:product_id>', views.product, name='cart_remove')
+    path('cart_add/<slug:product_slug>', views.cart_add, name='cart_add'),
+    path('cart_change/<slug:product_slug>', views.cart_change, name='cart_change'),
+    path('cart_remove/<slug:product_slug>', views.cart_remove, name='cart_remove')
 ]
