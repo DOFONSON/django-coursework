eabab0533142073d732ba56849f27ffa2b074a3d
D3k1 <dofodofonson@gmail.com>
2024-06-09 12:29:00 +0300

    users table added

--- BJGeDrenouille/BJGeDrenouille/settings.py
+++ BJGeDrenouille/BJGeDrenouille/settings.py
@@ -39,7 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'django.contrib.postgres',
     'main',
-    'goods'
+    'goods',
+    'users'
 ]
 
 MIDDLEWARE = [
@@ -57,7 +58,7 @@ ROOT_URLCONF = 'BJGeDrenouille.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
